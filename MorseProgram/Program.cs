b1305f2c7982a47a14235767b4fd48a22c683bea
idroj1 <mejiajordi@yahoo.com>
2021-10-07 23:26:07 -0400

    rushed 1rst try

---
+++ MorseProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MorseJordi;
+using System.IO;
+
+namespace MorseProgram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CoderTranslate Transformar = new CoderTranslater();
+            string texto = "Codigo Morse";
+        }
+    }
+}
